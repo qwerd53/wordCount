b043073bd4c5757953d441a1f319cd7abfbfa055
qwerd53 <2562820874@qq.com>
2023-05-01 11:13:50 +0800

    注释

---
+++ wordCount.py
@@ -0,0 +1,13 @@
+filename1 = 'D:\python\mycode\wordCount\-c input.txt'
+# Counting characters
+with open(filename1, 'r',encoding='utf-8') as f:
+    text = f.read()
+    num_chars = len(text)
+print(f'The number of characters in the file is: {num_chars}')
+filename2 = 'D:\python\mycode\wordCount\-w input.txt'
+# Counting words
+with open(filename2, 'r',encoding='utf-8') as f:
+    text = f.read()
+    num_words = len(text.split())
+print(f'The number of words in the file is: {num_words}')
+stop=input("Press <enter>")
